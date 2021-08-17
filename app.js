20201d5d99fc183df4ecdf065b8a37e23d9dfd7b
MohtashimAli85 <mohtashima85@gmail.com>
2021-08-17 16:18:39 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,82 @@
 
 const btn = document.querySelectorAll('.btn');
-const screen  = document.querySelector('.screen');
+const screen = document.querySelector('.screen');
+const operators = ['+', '-', 'x', '/'];
+let a = 0, b = 0, o;
+// Listening to Buttons CLick
 btn.forEach(
-    e =>{
-        e.addEventListener('click', x=>{
-            if(Number(x.target.textContent)){
-            screen.innerHTML +=
-             x.target.textContent;
+    e => {
+        e.addEventListener('click', x => {
+            if (screen.innerHTML.length >= 12) {
+                alert('No more numbers');
+                return;
+            } else if (Number(x.target.textContent)) {
+                screen.innerHTML += x.target.textContent;
+            } else if (x.target.textContent === 'DEL') {
+                let value = screen.innerHTML;
+                value = value.substring(0, value.length - 1);
+                screen.innerHTML = value;
+            } else if (x.target.textContent === 'RESET') {
+                screen.innerHTML = '';
+            } else if (x.target.innerHTML === '=') {
+                if (a != 0) {
+                    b = Number(screen.innerHTML);
+                    screen.innerHTML = '';
+                    if (o === '+') {
+                        screen.innerHTML = a + b;
+                    } else if (o === '-') {
+                        screen.innerHTML = a - b;
+                    } else if (o === '*') {
+                        screen.innerHTML = a * b;
+                    } else if (o === '/') {
+                        screen.innerHTML = a / b;
+                    }
+                    a = 0; b = 0; o = '';
+                }
             }
-        })
+
+
+            operators.forEach(e => {
+                if (e === x.target.textContent) {
+                    a = Number(screen.innerHTML);
+                    screen.innerHTML = '';
+                    o = e;
+                }
+            })
+        });
+    });
+// Listening to KeyPress 
+window.addEventListener('keypress', e => {
+    if (screen.innerHTML.length >= 12) {
+        alert('No more numbers');
+        return;
+    } else if (Number(e.key)) {
+        screen.innerHTML += e.key;
     }
-)
-const delBtn = document.querySelector('.btnDel');
-delBtn.addEventListener('click', e=>{
-    let value = screen.innerHTML;
-    value = Array.from(value);
-    value.pop();
-     
-     screen.innerHTML = value;
+});
+
+window.addEventListener('keypress', e => {
+    if (e.key === 'd') {
+        let value = screen.innerHTML;
+        value = value.substring(0, value.length - 1);
+        screen.innerHTML = value;
+    }
+});
+
+
+
+const body = document.querySelector('body');
+const tgBtn = document.querySelector('.tgBtn');
+// Listening to toggle button
+tgBtn.addEventListener('click', e => {
+    if (body.classList.contains('light')) {
+
+        body.classList.toggle('violet');
+        body.classList.remove('light');
+    } else if (body.classList.contains('violet')) {
+        body.classList.remove('violet');
+
+    } else {
+        body.classList.add('light');
+    }
 })
